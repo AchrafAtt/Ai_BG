c03572bad03c926bd94856dc1b40da12d1ceb4ff
AchrafAtt <attouaki.officiel@gmail.com>
2025-12-11 14:48:38 +0100

    handel api chatbot

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,13 +1,22 @@
 import express from 'express';
 import { Request, Response } from 'express';
-import { disposeEmitNodes } from 'typescript';
 import dotenv from 'dotenv';
 import OpenAI from 'openai';
+import z from 'zod';
 
 dotenv.config();
 
 const client = new OpenAI({
    apiKey: process.env.OPENAI_API_KEY,
+});
+
+const chatSchema = z.object({
+   prompt: z
+      .string()
+      .trim()
+      .min(1, 'Prompt is required')
+      .max(1000, 'Prompt is too long'),
+   conversationId: z.string().uuid(),
 });
 
 const app = express();
@@ -22,15 +31,29 @@ app.get('/api/test', (req: Request, res: Response) => {
    res.json({ message: 'Test endpoint is working!' });
 });
 
+const converations = new Map<string, string>();
+
 app.post('/api/chat', async (req: Request, res: Response) => {
-   const { prompt } = req.body;
-   const response = await client.responses.create({
-      model: 'gpt-4o-mini',
-      input: prompt,
-      temperature: 0.4,
-      max_output_tokens: 100,
-   });
-   res.json({ message: response.output_text });
+   const parseResult = chatSchema.safeParse(req.body);
+   if (!parseResult.success) {
+      return res.status(400).json(parseResult.error.format());
+   }
+   try {
+      const { prompt, conversationId } = req.body;
+      const response = await client.responses.create({
+         model: 'gpt-4o-mini',
+         input: prompt,
+         temperature: 0.4,
+         max_output_tokens: 100,
+         previous_response_id: converations.get(conversationId) || undefined,
+      });
+      converations.set(conversationId, response.id);
+      res.json({ message: response.output_text, responseID: response.id });
+   } catch (error) {
+      res.status(500).json({
+         error: 'An error occurred while processing your request.',
+      });
+   }
 });
 
 app.listen(PORT, () => {
